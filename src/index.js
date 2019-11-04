a3cadf0d6874fca4e2047b3e9d002d0dfe4a86b0
Max Schmitt <max@schmitt.mx>
2019-11-04 23:19:25 +0100

    fix: mobile responsibe hamburger menu

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import ClipboardJS from 'clipboard'
 
 document.addEventListener("DOMContentLoaded", () => {
   new ClipboardJS("#copy-btn");
+
+  const burgerBtn = document.getElementById("burger-btn")
+  const mainNavbar = document.getElementById("navbar-main")
+  burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('is-active');
+    mainNavbar.classList.toggle('is-active');
+  })
 })
