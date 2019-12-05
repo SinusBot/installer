a465cac7cd1de036070d1287dae5e21889c5009d
Max Schmitt <max@schmitt.mx>
2019-12-05 12:15:20 +0100

    feat: add toast after successfull copy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import ClipboardJS from 'clipboard'
+import { toast } from 'bulma-toast'
 
 document.addEventListener("DOMContentLoaded", () => {
-  new ClipboardJS("#copy-btn");
+  const clipboard = new ClipboardJS("#copy-btn");
+
+  clipboard.on('success', function (e) {
+    toast({
+      message: "Copied command to clipboard!",
+      type: "is-success",
+      position: "bottom-center"
+    })
+  });
 
   const burgerBtn = document.getElementById("burger-btn")
   const mainNavbar = document.getElementById("navbar-main")
   burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('is-active');
     mainNavbar.classList.toggle('is-active');
+  })
+
+  const copyElement = document.getElementById("copy-input")
+  copyElement.addEventListener("focus", () => {
+    copyElement.select()
   })
 })
